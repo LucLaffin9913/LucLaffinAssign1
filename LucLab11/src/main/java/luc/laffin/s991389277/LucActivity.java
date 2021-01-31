709254c5d1893cb1988ace61738562e2d2e05790
laffinl <laffinl@sheridancollege.ca>
2021-01-31 10:52:20 -0500

    Initial Commit

---
+++ LucLab11/src/main/java/luc/laffin/s991389277/LucActivity.java
@@ -0,0 +1,20 @@
+/*
+Luc Laffin s991389277
+Assignment 1 to demonstrate the use of
+Activities and how to invoke 3rd party app
+ */
+
+package luc.laffin.s991389277;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LucActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
