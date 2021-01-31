70e29325e6462a664a9aa1abd310f20f889a8176
laffinl <laffinl@sheridancollege.ca>
2021-01-31 12:06:18 -0500

    Added Welcome Label, Input Text Box, and Two Buttons to first Activity Added Toast and URL to first button

--- LucLab11/src/main/java/luc/laffin/s991389277/LucActivity.java
+++ LucLab11/src/main/java/luc/laffin/s991389277/LucActivity.java
@@ -8,7 +8,12 @@ package luc.laffin.s991389277;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class LucActivity extends AppCompatActivity {
 
@@ -16,5 +21,26 @@ public class LucActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void buttonClick(View view) {
+        Intent intent = null;
+        switch (view.getId()) {
+            case R.id.lucB1:
+                Context context = getApplicationContext();
+                CharSequence text = "Opening Data Driven Strength website";
+                int duration = Toast.LENGTH_LONG;
+                Toast toast = Toast.makeText(context, text, duration);
+                toast.show();
+
+                intent = new Intent(Intent.ACTION_VIEW,
+                        Uri.parse("https://www.data-drivenstrength.com"));
+                startActivity(intent);
+                break;
+            case R.id.lucB2:
+
+            default:
+                break;
+        }
     }
 }
