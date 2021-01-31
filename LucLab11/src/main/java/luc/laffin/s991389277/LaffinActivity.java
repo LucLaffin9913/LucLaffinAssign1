ec6254cce2937e0f00efdd9ce43b04a2448507d1
laffinl <laffinl@sheridancollege.ca>
2021-01-31 12:59:37 -0500

    Added Image to 2nd Activity Added Button on 2nd Activity to return to 1st Activity

--- LucLab11/src/main/java/luc/laffin/s991389277/LaffinActivity.java
+++ LucLab11/src/main/java/luc/laffin/s991389277/LaffinActivity.java
@@ -8,7 +8,14 @@ package luc.laffin.s991389277;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.material.snackbar.Snackbar;
 
 public class LaffinActivity extends AppCompatActivity {
 
@@ -16,5 +23,17 @@ public class LaffinActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_laffin);
+    }
+
+    public void buttonClick(View view) {
+        Intent intent = null;
+        switch (view.getId()) {
+            case R.id.lucB3:
+                intent = new Intent(this, LucActivity.class);
+                startActivity(intent);
+                break;
+            default:
+                break;
+        }
     }
 }
